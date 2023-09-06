267cf808e0ebfbdbb2bedbf01896bf6ebef644ab
Igor Cunha <igordscunha@outlook.com>
2023-09-06 20:40:47 -0300

    Testando email 2

--- src/pages/api/registro.js
+++ src/pages/api/registro.js
@@ -5,20 +5,25 @@ import db from '../../config/dbConnect.js'
 export default async function handler(req, res) {
 
   db //mongoose.connection
-
+  
   if (req.method === 'POST') {
     try {
       const leadCadastrado = new Leads(req.body)
       console.log('Requisição POST realizada com sucesso...')
       res.status(201).json({ message: 'Tudo certo' })
-      await leadCadastrado.save()
-      sendGridMail.setApiKey(process.env.SENDGRID_KEY)
-      await sendGridMail.send({
-        from: 'hugo@pontesebritto.adv.br',
-        to: leadCadastrado.email,
-        subject: 'Seja bem vindx, Companheirx!',
-        html: `<strong>Olá, camaradx ${leadCadastrado.nome}</strong>.<br><br><br> Muito obrigado por fazer parte da minha newsletter! <br><br> Toda terça-feira, às 08:00 em ponto, reunirei todo o conteúdo da semana e ele chegará no seu e-mail contendo as últimas notícias relevantes, opiniões pessoais, indicações e muito mais... <br><br> Então, mais uma vez, obrigado e um grande abraço do Hugo! <br><br><br><br> @advogadosocialista`
+      await leadCadastrado.save().then(() => {
+        sendGridMail.setApiKey(process.env.SENDGRID_KEY)
+        sendGridMail.send({
+          from: 'hugo@pontesebritto.adv.br',
+          to: leadCadastrado.email,
+          subject: 'Seja bem vindx, Companheirx!',
+          html: `<strong>Olá, camaradx ${leadCadastrado.nome}</strong>.<br><br><br> Muito obrigado por fazer parte da minha newsletter! <br><br> Toda terça-feira, às 08:00 em ponto, reunirei todo o conteúdo da semana e ele chegará no seu e-mail contendo as últimas notícias relevantes, opiniões pessoais, indicações e muito mais... <br><br> Então, mais uma vez, obrigado e um grande abraço do Hugo! <br><br><br><br> @advogadosocialista`
+        })
+      }).catch((erro) => {
+        console.log(erro)
+        res.status(statusCode).json({error: 'Email não enviado'})
       })
+      
     } catch (error) {
       console.log(error)
       res.status(400).json({error: 'Alguma coias deu errado na requisição POST...'})
